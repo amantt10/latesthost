7e896a7061bf3906978de5710671e141037b6e9d
Aman <92225430+amantt10@users.noreply.github.com>
2025-05-08 05:43:36 -0500

    Add files via upload

---
+++ _next/static/chunks/app/Adminpage/page-fdd57fea0cedfe00.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[227,1512,3829,4013,4036,7074,9667],{36764:(e,s,n)=>{Promise.resolve().then(n.bind(n,39946)),Promise.resolve().then(n.t.bind(n,33063,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[7244,4140,9946,8441,1684,7358],()=>s(36764)),_N_E=e.O()}]);
---
+++ _next/static/chunks/app/pages/NewsEvents/NewsUpdates/page-8933b807fd6f43c9.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[5936],{97623:(e,s,n)=>{Promise.resolve().then(n.bind(n,39946)),Promise.resolve().then(n.t.bind(n,6874,23)),Promise.resolve().then(n.t.bind(n,33063,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[7244,4140,9946,8441,1684,7358],()=>s(97623)),_N_E=e.O()}]);
